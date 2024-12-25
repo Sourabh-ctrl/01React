aa5d531b9f2bbeb30de932ee6aa9f9d518c18620
Sourabh Lathi <lathisaurav@gmail.com>
2024-12-25 13:55:05 +0530

    first commit

---
+++ src/config/gemini.js
@@ -0,0 +1,37 @@
+// const apiKey ;
+
+
+import {
+  GoogleGenerativeAI,
+  HarmCategory,
+  HarmBlockThreshold,
+} from "@google/generative-ai";
+
+const apiKey = ("AIzaSyDAhX9yBeZ_DMSA0TYQ7l7sE4VzSXkSxpk");
+const genAI = new GoogleGenerativeAI("AIzaSyDAhX9yBeZ_DMSA0TYQ7l7sE4VzSXkSxpk");
+
+const model = genAI.getGenerativeModel({
+  model: "gemini-2.0-flash-exp",
+});
+
+const generationConfig = {
+  temperature: 1,
+  topP: 0.95,
+  topK: 40,
+  maxOutputTokens: 8192,
+  responseMimeType: "text/plain",
+};
+
+async function run(prompt) {
+  const chatSession = model.startChat({
+    generationConfig,
+    history: [
+    ],
+  });
+
+  const result = await chatSession.sendMessage(prompt);
+  console.log(result.response.text());
+  return result.response.text();
+}
+
+export default run;
